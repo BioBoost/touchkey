c5859844fb87d8e9d5b0cc22f912af44760c23b1
Nico De Witte <nico.dewitte@outlook.com>
2019-09-04 22:47:24 +0200

    Add constructor

--- src/touchkey.js
+++ src/touchkey.js
@@ -1,1 +1,12 @@
-console.log("Not yet implemented");
+const Qt1070 = require('qt1070');
+const i2c = require('i2c-bus');
+
+class TouchKey {
+
+  constructor(i2c) {
+    this.touch = new Qt1070(i2c);
+  }
+
+}
+
+module.exports = TouchKey;
