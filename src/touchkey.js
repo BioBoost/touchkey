12b2934b2ac66a9409d0bf544afd2e76ec18620c
Nico De Witte <nico.dewitte@outlook.com>
2019-09-04 22:54:17 +0200

    Create interrupt pin

--- src/touchkey.js
+++ src/touchkey.js
@@ -1,10 +1,12 @@
 const Qt1070 = require('qt1070');
 const i2c = require('i2c-bus');
+const Gpio = require('onoff').Gpio;
 
 class TouchKey {
 
-  constructor(i2c) {
+  constructor(i2c, interruptPin=4) {
     this.touch = new Qt1070(i2c);
+    this.interrupt = new Gpio(interruptPin, 'in', 'falling');
   }
 
 }
