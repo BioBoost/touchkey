4f168e9e35b82c4f03c8b2d02c15467b3faba1f0
Nico De Witte <nico.dewitte@outlook.com>
2019-09-04 22:57:47 +0200

    Catch interrupts from qt and fetch key state

--- src/touchkey.js
+++ src/touchkey.js
@@ -7,6 +7,28 @@ class TouchKey {
   constructor(i2c, interruptPin=4) {
     this.touch = new Qt1070(i2c);
     this.interrupt = new Gpio(interruptPin, 'in', 'falling');
+    this.read_full_status();    // Clears interrupt flags
+    this.start_watching_for_keys();
+  }
+
+  //////////////////////
+  // Internal methods //
+  //////////////////////
+
+  read_full_status() {
+    return {
+      keys: this.touch.keys(),
+      status: this.touch.status()
+    }
+  }
+
+  start_watching_for_keys() {
+    this.interrupt.watch((err, value) => {
+      if (!err) {
+        let keys = this.read_full_status().keys;
+        console.log(`Keys: ${keys}`);
+      }
+    });
   }
 
 }
